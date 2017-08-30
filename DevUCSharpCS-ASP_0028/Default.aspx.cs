122772c34d36a2d79f6af53e359cbb52e5f9f263
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-30 14:36:41 -0500

    added helper method to calc # of cups

--- DevUCSharpCS-ASP_0028/Default.aspx.cs
+++ DevUCSharpCS-ASP_0028/Default.aspx.cs
@@ -14,65 +14,45 @@ namespace DevUCSharpCS_ASP_0028
 
         }
 
+        //helper method to calculate cups
+        private void calculateCups()
+        {
+            if (quantityTextBox.Text.Trim().Length == 0)
+                return;
+
+            double quantity = 0.0;
+            if (!Double.TryParse(quantityTextBox.Text, out quantity))
+                return;
+
+            double cups = 0.0;
+
+            if (fromCupsRadio.Checked) cups = quantity;
+            else if (fromPintsRadio.Checked) cups = quantity * 2;
+            else if (fromQuartsRadio.Checked) cups = quantity * 4;
+            else if (fromGallonsRadio.Checked) cups = quantity * 16;
+
+            resultLabel.Text = "The number of cups: " + cups.ToString();
+
+        }
+
         protected void fromCupsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            //if the text box is empty once all white space is removed..
-            if (quantityTextBox.Text.Trim().Length == 0)
-                //exit the method
-                return;
-
-            //set quantity to 0.0
-            double quantity = 0.0;
-            //if whatever's in the text box can't be parsed into a double
-            if (!Double.TryParse(quantityTextBox.Text, out quantity))
-                //exit the method
-                return;
-
-            //otherwise set the text box to....
-            resultLabel.Text = "The number of cups: " + quantity.ToString();
- 
+            calculateCups();
         }
 
         protected void fromPintsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            if (quantityTextBox.Text.Trim().Length == 0)
-                return;
-
-            double quantity = 0.0;
-            if (!Double.TryParse(quantityTextBox.Text, out quantity))
-                return;
-
-            double cups = quantity * 2.0;
-            resultLabel.Text = "The number of cups: " + quantity.ToString();
-
+            calculateCups();
         }
 
         protected void fromQuartsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            if (quantityTextBox.Text.Trim().Length == 0)
-                return;
-
-            double quantity = 0.0;
-            if (!Double.TryParse(quantityTextBox.Text, out quantity))
-                return;
-
-            double cups = quantity * 4.0;
-            resultLabel.Text = "The number of cups: " + quantity.ToString();
-
+            calculateCups();
         }
 
         protected void fromGallonsRadio_CheckedChanged(object sender, EventArgs e)
         {
-            if (quantityTextBox.Text.Trim().Length == 0)
-                return;
-
-            double quantity = 0.0;
-            if (!Double.TryParse(quantityTextBox.Text, out quantity))
-                return;
-
-            double cups = quantity * 16;
-            resultLabel.Text = "The number of cups: " + quantity.ToString();
-
+            calculateCups();
         }
 
         protected void quantityTextBox_TextChanged(object sender, EventArgs e)
